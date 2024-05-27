b9335dcf82c19b0a7750e43c0ef66ebdc9612b6c
Alexandra Sicoe <sicoealexandra97@gmail.com>
2024-05-27 15:46:29 +0300

    Create Bookings with post

--- src/Components/BookingCard.jsx
+++ src/Components/BookingCard.jsx
@@ -2,24 +2,91 @@ import * as React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
+import { Button, Input, Typography } from "@mui/material";
 import { CardActionArea } from "@mui/material";
 import CardCover from "../images/CardCover.jpg";
+import axios from "axios";
+import { useState } from "react";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+
 export default function ActionAreaCard() {
+  const [location, setLocation] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+
+  const getBooking = async () => {
+    try {
+      const token = localStorage.getItem("token");
+
+      const response = await axios.post(
+        "http://localhost:3012/api/bookings",
+        {
+          location: location,
+          startDate: startDate,
+          endDate: endDate,
+        },
+        {
+          headers: { Authorization: "Bearer " + token },
+        }
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card
+      sx={{
+        maxWidth: 345,
+      }}
+    >
       <CardActionArea>
         <CardMedia component="img" image={CardCover} height="140" />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Booking
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
-            eveniet nam dolor quae nihil unde tempora atque omnis impedit eaque
-            quaerat, expedita veritatis sequi optio aliquam, aperiam odio dicta
-            sapiente.
-          </Typography>
+          <form
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Input
+              type="text"
+              placeholder="Location"
+              required
+              value={location}
+              fullWidth
+              sx={{ mb: 2 }}
+              inputProps={{ style: { color: "#1a237e" } }}
+              onChange={(e) => setLocation(e.target.value)}
+            />
+            <Typography level="title-md">Start Date</Typography>
+            <DatePicker onChange={(date) => setStartDate(date)} />
+            <Typography
+              sx={{
+                marginTop: "15px",
+              }}
+              level="title-md"
+            >
+              End Date
+            </Typography>
+            <DatePicker onChange={(date) => setEndDate(date)} />
+            <Button
+              onClick={() => {
+                getBooking();
+              }}
+              sx={{
+                backgroundImage:
+                  "linear-gradient(90deg, #1a237e 0%, #000000 100%)",
+                color: "white",
+                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.3)",
+                marginTop: "15px",
+              }}
+            >
+              Create Booking
+            </Button>
+          </form>
         </CardContent>
       </CardActionArea>
     </Card>
--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,18 @@ import BookingSystem from "./Pages/BookingSystem";
 import LoginRegisterPage from "./Pages/LoginRegisterPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegisterForm from "./Components/RegisterForm";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<BookingSystem />} />
-        <Route path="/management" element={<LoginRegisterPage />} />
-        <Route path="/login" element={<RegisterForm />} />
-      </Routes>
-    </BrowserRouter>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<BookingSystem />} />
+          <Route path="/management" element={<LoginRegisterPage />} />
+          <Route path="/login" element={<RegisterForm />} />
+        </Routes>
+      </BrowserRouter>
+    </LocalizationProvider>
   </React.StrictMode>
 );
